788496b48b3ac7bcede2d809486d5407b18004a4
Joel Ross <joelross@uw.edu>
2018-06-01 13:21:01 -0700

    Rewrite exercise-1 for clarity

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,53 +2,54 @@
 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
-library(httr)
-library(jsonlite)
-library(dplyr)
-
-# Create a variable for the API's base URI (https://api.github.com)
 
 
-# Under the "Repositories" category of the API, 
-# find the endpoint that will list repos in an organization. Then, 
-# Create a variable `resource` that stores the endpoint for the "info201"
-# organization repos (this is the PATH to the resource of interest).
-# (FYI: this is where we keep the book code and master exercise sets!)
+# Create a variable base_uri that stores the base URI (as a string) for the 
+# Github API (https://api.github.com)
 
 
-# Send a GET request to this endpoint (the `base_uri`` followed by `resource`)
+# Under the "Repositories" category of the API documentation, find the endpoint 
+# that will list _repos in an organization_. Then create a variable named
+# `org_resource` that stores the endpoint for the `info201` organization repos 
+# (this is the _path_ to the resource of interest).
 
 
-# Extract the "text" of the response usin the `content` function
+# Send a GET request to this endpoint (the `base_uri` followed by the 
+# `org_resource` path). Print the response to show that your request worked. 
+# (The listed URI will also allow you to inspect the JSON in the browser easily).
 
 
-# Convert the body from JSON into a data frame
+# Extract the content of the response using the `content()` function, saving it
+# in a variable.
+
+
+# Convert the content variable from a JSON string into a data frame.
 
 
 # How many (public) repositories does the organization have?
 
 
-##### New query ######
-
-# Use a "Search" endpoint to search for repositories about "graphics"
-# (bonus: limit language to only "R" -- which requires a different syntax)
-# (hint: https://developer.github.com/v3/search/#search-repositories)
-# Reassign the `resource` variable to refer to the appropriate resource.
+# Now a second query:
+# Create a variable `search_endpoint` that stores the endpoint used to search 
+# for repositories. (Hint: look for a "Search" endpoint in the documentation).
 
 
-# You will need to specify some query parameters. Create a `query_params` list 
-# variable that specifies an appropriate key and value for the search term and
-# the language
+# Search queries require a query parameter (for what to search for). Create a 
+# `query_params` list variable that specifies an appropriate key and value for 
+# the search term
 
 
-# Send a GET request to this endpoint--including your params list as the `query`
+# Send a GET request to the `search_endpoint`--including your params list as the
+# `query`. Print the response to show that your request worked.
 
 
-# Extract the response body and convert it from JSON.
+# Extract the content of the response and convert it from a JSON string into a
+# data frame. 
 
 
-# How many search repos did your search find? (Hint: check the list names)
+# How many search repos did your search find? (Hint: check the list names to 
+# find an appropriate value).
 
 
-# What are the full names of the top 5 results?
+# What are the full names of the top 5 repos in the search results?
 
