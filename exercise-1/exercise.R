a08719dd60d3ac0caf00ce7f397d42910ad566c8
Joel Ross <joelross@uw.edu>
2018-06-01 14:35:45 -0700

    Update exercise and style fixes

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,9 +2,9 @@
 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
-library(httr)
-library(jsonlite)
-library(dplyr)
+library("httr")
+library("jsonlite")
+library("dplyr")
 
 # Create a variable base_uri that stores the base URI (as a string) for the 
 # Github API (https://api.github.com)
@@ -24,10 +24,10 @@ print(response)
 
 # Extract the content of the response using the `content()` function, saving it
 # in a variable.
-body <- content(response, "text")
+response_text <- content(response, "text")
 
 # Convert the content variable from a JSON string into a data frame.
-org_repos <- fromJSON(body)
+org_repos <- fromJSON(response_text)
 
 # How many (public) repositories does the organization have?
 print(nrow(org_repos))
@@ -35,12 +35,12 @@ print(nrow(org_repos))
 # Now a second query:
 # Create a variable `search_endpoint` that stores the endpoint used to search 
 # for repositories. (Hint: look for a "Search" endpoint in the documentation).
-search_endpoint <- '/search/repositories'
+search_endpoint <- "/search/repositories"
 
 # Search queries require a query parameter (for what to search for). Create a 
 # `query_params` list variable that specifies an appropriate key and value for 
-# the search term (you can search for anything you want!)
-query_params <- list(q = "visualization")
+# the search term
+query_params <- list(q = "graphics")
 
 # Send a GET request to the `search_endpoint`--including your params list as the
 # `query`. Print the response to show that your request worked.
@@ -49,13 +49,13 @@ print(response)
 
 # Extract the content of the response and convert it from a JSON string into a
 # data frame. 
-body <- content(response, "text")
-results <- fromJSON(body)
+response_text <- content(response, "text")
+graphics_repos = fromJSON(response_text)
 
 # How many search repos did your search find? (Hint: check the list names to 
 # find an appropriate value).
-print(results$total_count)
+print(graphics_repos$total_count)
 
 # What are the full names of the top 5 repos in the search results?
-vis_repo_names <- results$items$full_name[1:5]
-print(vis_repo_names)
+graphics_repo_names <- graphics_repos$items$full_name[1:5]
+print(graphics_repo_names)
